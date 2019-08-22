77614013a34de32e2a72da21ad4272235cafbd01
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-08-22 05:38:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   counter = 0
-
+  new_array = []
   while counter < array.length
-    array[counter] ** 2
+    array[counter] ** 2<< new_array
     counter += 1
   end
 end
